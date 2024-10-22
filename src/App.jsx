746943dfd6809c058ebee7b8e3cc01bc5eef12d6
Mia Setya <89637710+miasetya12@users.noreply.github.com>
2024-10-22 22:49:19 +0800

    yyy

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Welcome to My Simple Web App!</h1>
+      <h1>Welcome to My Mia web!</h1>
       <p>This is a basic page built with React and Vite.</p>
     </div>
   );
